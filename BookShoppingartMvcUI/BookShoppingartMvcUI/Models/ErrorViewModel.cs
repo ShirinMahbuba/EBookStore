c2eaad16694cda09368d1270162d45de7042a656
ShirinMahbuba <mahbubasara3@gmail.com>
2025-12-31 21:23:14 +0600

    fixed gitignore

---
+++ BookShoppingartMvcUI/BookShoppingartMvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShoppingartMvcUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ BookShoppingartMvcUI/BookShoppingartMvcUI/Models/OrderDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingartMvcUI.Models
+{
+    [Table ("OrderDetails")]
+    public class OrderDetails
+    {
+        public int Id { get; set; }
+        [Required]
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        [Required]
+        public int BookId { get; set; }
+
+        public Book Book { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public double  UnitPrice { get; set; }
+    }
+}
